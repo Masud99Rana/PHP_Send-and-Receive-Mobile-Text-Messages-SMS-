6e696ccb7876f4ade137dc294e87283e25b81835
Masud99Rana <Masudrana917775@gmail.com>
2019-08-09 22:52:52 +0600

    Verifying accounts success

--- public/code.php
+++ public/code.php
@@ -14,30 +14,25 @@
     $_SESSION['code'] = $code = uniqid();
     
     $con->query("INSERT INTO verifications(code) VALUES('$code')");
+
+
+
+    try {
+    	
+		$client->messages->create(
+		    // Where to send a text message (your cell phone?)
+		    '+8801885544345',
+		    array(
+		        'from' => $config['phone_number'],
+		        'body' => $code
+		    )
+		);
+    } catch (Services_Twilio_RestException $e) {
+
+    	die('sorry Could not connect because'.$e->getMessage());
+    	
+    }
 	
-	$client->messages->create(
-	    // Where to send a text message (your cell phone?)
-	    '+8801885544345',
-	    array(
-	        'from' => $config['phone_number'],
-	        'body' => $code
-	    )
-	);
-
-
-
-
-    // try{
-
-    //     $client->account->messages->sendMessage($config['phone_number'],'5397771210', $code);
-
-
-    // } catch(Services_Twilio_RestException $e){
-
-    //     die('Sorry could not connect because it could not ' . $e->getMessage());
-
-
-    // }
 
 
     echo "<h3 class='text-center bg-success'>Your Code has been sent</h3>";
--- public/verify.php
+++ public/verify.php
@@ -1,11 +1,69 @@
 <?php require '../includes/header.php'?>
 
 
+<?php
 
-<h1>Verify Page</h1>
+
+    if(isset($_POST['submit'])) {
+
+        if(isset($_POST['code'])){
+
+
+            $code = $_SESSION['code'];
+
+
+            $res = $con->query("SELECT * FROM verifications WHERE code='{$code}'");
+            $row = $res->fetch_assoc();
+
+            $row['code'];
+
+
+            if(trim($_POST['code']) == $row['code']){
+
+                $con->query("DELETE FROM verifications WHERE code='{$code}'");
+
+                header("Location: success.php");
+
+
+            } else {
+
+                echo "<div class='row'>";
+
+                echo "<h3 class='bg-danger text-center'> Wrong code, please try again</h3>";
+
+                echo "</div>";
+
+
+            }
+
+        }
+
+
+    }
+
+?>
+
+
+
+<div class="row">
+
+    <div class="col-sm-6 col-sm-offset-3">
+
+
+        <form role="form" method="post">
+            <div class="form-group">
+
+                <input name="code" type="text" class="form-control" id="code" placeholder="Enter code">
+            </div>
+
+            <input name="submit" type="submit" class="btn btn-primary btn-block" value="Verify">
+        </form>
+
+
+    </div>
+</div>
+
 
 
 
 <?php require '../includes/footer.php'?>
-
-
