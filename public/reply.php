b948e76b45acfdb9ca4d8d69ac8c60f6f388217b
Masud99Rana <Masudrana917775@gmail.com>
2019-08-09 23:18:37 +0600

    error exception added

--- public/reply.php
+++ public/reply.php
@@ -1,11 +1,40 @@
-<?php require '../includes/header.php'?>
+<?php
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+
+
+<?php
+
+    $people  = array(
+
+        '+16455767473' => 'Maria Santiago',
+        '+15455767473' => 'Jose Santiago',
+        '+13455767473' => 'Peter Williams',
+        '+11455767473' => 'John Doe',
+        '+15397771210' => 'Edwin Diaz',
+
+
+);
+
+
+    if(!$name = $people[$_REQUEST['From']]){
+
+
+        $name = "nobody";
+
+
+    }
+
+
+?>
 
 
 
-<h1>Reply Page</h1>
+<Response>
 
 
+    <Sms><?php echo "Hi ". $name . " How are you? Thanks for calling" ?> </Sms>
 
-<?php require '../includes/footer.php'?>
 
-
+</Response>
